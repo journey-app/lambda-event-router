3c0db1c0b1a125c787f02050de63011728193ee3
wpc <alex.hal9000@gmail.com>
2016-04-04 17:16:35 -0700

    add license header

--- test/expected_output.js
+++ test/expected_output.js
@@ -1,1 +1,16 @@
+/***************************************************************************
+ * Copyright 2016 ThoughtWorks, Inc. and Pengchao Wang
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **************************************************************************/
 module.exports = [{"token":"j.25af16a2-df03-4b34-8f45-a8813aed4672","events":[{"type":"track","name":"click|basic edition|button","properties":{"_locator":"INPUT"},"id":"62201764-7ff7-8c91-2f82-1c887f825db5","uid":null,"anonymousId":"7993751d-61e8-a1e8-1c9f-f2ddd0d53c56","at":"2015-09-25T00:48:10.956Z"},{"type":"track","name":"click|plus edition|button","properties":{"_locator":"INPUT"},"id":"e4da0b73-9c60-7466-12d9-8f442c6765ac","uid":null,"anonymousId":"7993751d-61e8-a1e8-1c9f-f2ddd0d53c56","at":"2015-09-25T00:48:11.317Z"}]}]
